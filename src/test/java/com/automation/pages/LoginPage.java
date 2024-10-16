1567659b95c2c6dfafa8bfb9974b459280bc942d
FouziyaBanu <fouziya.banu@zucitech.com>
2024-10-16 17:51:49 +0530

    first

--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-    @FindBy(id="username")
+    @FindBy(id="usernametets")
     private WebElement untbx;
     @FindBy(name="pw")
     private WebElement pwtb;
